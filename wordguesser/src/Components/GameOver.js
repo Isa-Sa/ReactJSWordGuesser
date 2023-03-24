d6a78ea7fae3884b48aa888835274f51d2d833cf
Isa-Sa <112902225+Isa-Sa@users.noreply.github.com>
2023-03-24 22:23:08 +0200

    Add files via upload

---
+++ wordguesser/src/Components/GameOver.js
@@ -0,0 +1,29 @@
+import React, { useContext } from "react";
+import { AppContext } from "../App";
+
+function GameOver() {
+  const {
+    board,
+    setBoard,
+    currAttempt,
+    gameOver,
+    onSelectLetter,
+    correctWord,
+    onDelete,
+  } = useContext(AppContext);
+  return (
+    <div className="gameOver">
+      <h3>
+        {gameOver.guessedWord
+          ? "Congratulations! 🥳 The word is correct."
+          : "That's not right 🥺 try again!"}
+      </h3>
+      <h3>Correct Word: {correctWord}</h3>
+      {gameOver.guessedWord && (
+        <h3>You guessed in {currAttempt.attempt} attempts</h3>
+      )}
+    </div>
+  );
+}
+
+export default GameOver;
---
+++ wordguesser/src/Words.js
@@ -0,0 +1,23 @@
+import wordList from "./WordList.txt";
+
+export const boardDefault = [
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+  ["", "", "", "", ""],
+];
+
+export const generateWordSet = async () => {
+  let wordSet;
+  let todaysWord;
+  await fetch(wordList)
+    .then((response) => response.text())
+    .then((result) => {
+      const wordArr = result.split("\n");
+      todaysWord = wordArr[Math.floor(Math.random() * wordArr.length)];
+      wordSet = new Set(wordArr);
+    });
+  return { wordSet, todaysWord };
+};
